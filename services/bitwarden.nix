ba041987201fe819db234609893a5a7be306a5a5
Hannes Hilbert <git@hsht.de>
2024-12-17 22:23:57 +0100

    configs

--- services/bitwarden.nix
+++ services/bitwarden.nix
@@ -4,7 +4,12 @@
     enable = true;
   };
 
+  services.vaultwarden.config = {
+  ROCKET_ADDRESS = "0.0.0.0";
+  ROCKET_PORT = 8111;
+  }
+
   networking.firewall.allowedTCPPorts = [
-    8222
+    8111
   ];
 }
